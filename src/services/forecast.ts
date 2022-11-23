bf1ac5b7d8ac4dc4e65463e3a48e382fae2cea9d
Ryan de Menezes <menezesryan1010@gmail.com>
2022-11-23 11:21:07 -0300

    Integrando Beach e Forecast e mockando APIs com nock

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,20 +1,6 @@
 import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
+import { Beach } from '@src/models/Beach';
 import { InternalError } from '@src/util/errors/internal-error';
-
-export enum BeachPosition {
-  S = 'S',
-  E = 'E',
-  W = 'W',
-  N = 'N',
-}
-
-export interface Beach {
-  lat: number;
-  lng: number;
-  name: string;
-  position: BeachPosition;
-  user: string;
-}
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
