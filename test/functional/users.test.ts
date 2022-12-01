9e1240e226448420cc98553dec9197d1f6a226bb
Ryan de Menezes <menezesryan1010@gmail.com>
2022-12-01 14:49:59 -0300

    Iniciando o algoritmo de rating

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -133,4 +133,38 @@ describe('Users functional tests', () => {
       });
     });
   });
+
+  describe('When getting user profile info', () => {
+    it("Should return the token's owner profile information", async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const user = await new User(newUser).save();
+      const token = AuthService.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest.get('/users/me').set({
+        'x-access-token': token,
+      });
+
+      expect(status).toBe(200);
+      expect(body).toMatchObject(JSON.parse(JSON.stringify({ user })));
+    });
+
+    it('Should return Not Found, when the user is not found', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const user = await new User(newUser);
+      const token = AuthService.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest.get('/users/me').set({
+        'x-access-token': token,
+      });
+
+      expect(status).toBe(404);
+      expect(body.message).toBe('User not found!');
+    });
+  });
 });
